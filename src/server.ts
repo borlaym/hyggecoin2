97ca683621d27011a482a0266fcf72852e02a5ed
Marton Borlay <borlaym@gmail.com>
2021-12-28 15:05:37 +0100

    Add server

---
+++ src/server.ts
@@ -0,0 +1,31 @@
+import express, { ErrorRequestHandler } from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.use(bodyParser.json());
+
+app.use('*', (req, res, next) => {
+  res.setHeader('access-control-allow-origin', '*');
+  res.setHeader('access-control-allow-headers', '*');
+  next();
+})
+
+const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  console.log(err);
+  res.status(400);
+  res.send({
+    data: null,
+    error: err.message
+  });
+}
+
+app.use('*', errorHandler)
+
+const port = process.env.PORT || (process.argv.includes('--peer') ? Math.floor(Math.random() * 500 + 8000) : 9000);
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
